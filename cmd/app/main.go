627ef50883d58d38051e9900dc77f2383b0e35bc
SunsetLovelly <viktor.podgorbunskiyy@gmail.com>
2026-01-08 14:59:18 +0300

    start http server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,11 +9,21 @@ import (
 
 func main() {
 	// Регистрируем handler
-	http.HandleFunc("/news", news.GetAllNews)
+	http.HandleFunc("/news", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			news.GetAllNews(w, r)
+		case http.MethodPost:
+			news.CreateNews(w, r)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
 
+	// Логируем запуск сервера
 	log.Println("Server started on :8080")
 
-	// Запускаем сервер
+	// Запускаем HTTP сервер (блокирует выполнение)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
