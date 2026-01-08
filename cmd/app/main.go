0766debe9a9d793735aed6a205977898bce22139
SunsetLovelly <viktor.podgorbunskiyy@gmail.com>
2026-01-08 14:18:39 +0300

    connect news handler and start server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,21 @@
-package app
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"news-crud/internal/news"
+)
+
+func main() {
+	// Регистрируем handler
+	http.HandleFunc("/news", news.GetAllNews)
+
+	log.Println("Server started on :8080")
+
+	// Запускаем сервер
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- internal/news/handler.go
+++ internal/news/handler.go
@@ -1,1 +1,24 @@
-package news
+package news // логично, это модуль новостей)))
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// функция, которую вызывает cервер HTTP
+func GetAllNews(w http.ResponseWriter, r *http.Request) {
+	// Разрешаем только GET
+	if r.Method != http.MethodGet { // Делаем обязательно проверку! один handler = один метод (Защита от POST/PUT/DELETE)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	// Читаем просто данные
+	posts, err := LoadPosts()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	//Ответ клиенту
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(posts)
+}
